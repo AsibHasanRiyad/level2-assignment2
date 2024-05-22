1d1b687ae80ba1f2b58c561da1ae698088253091
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-05-23 02:41:32 +0600

    product quantity is decreasing  after order a product

--- src/modules/orders/orders.controllers.ts
+++ src/modules/orders/orders.controllers.ts
@@ -1,16 +1,42 @@
 import { Request, Response } from "express";
 import { OrderServices } from "./orders.service";
+import { ProductServices } from "../products/products.service";
+import { TProducts } from "../products/products.interface";
 
 // create a new order
 const createOrder = async (req: Request, res: Response) => {
   try {
     const orderData = req.body;
-    const result = await OrderServices.createOrder(orderData);
-    res.status(200).json({
-      success: true,
-      message: "Order created successfully!",
-      data: result,
-    });
+    console.log(req.body.productId);
+    const { productId, quantity } = req.body;
+
+    // check if product is available
+    const OrderingProduct = await ProductServices.getSingleProduct(productId);
+    const availableQuantity = OrderingProduct?.inventory.quantity;
+    // if not available
+    if (
+      (availableQuantity as number) < 1 ||
+      (availableQuantity as number) < quantity
+    ) {
+      return res.status(500).json({
+        success: false,
+        message: "Insufficient quantity available in inventory",
+      });
+    } else {
+      const result = await OrderServices.createOrder(orderData);
+      const newQuantity = (availableQuantity as number) - quantity;
+      console.log(OrderingProduct);
+      (OrderingProduct as TProducts).inventory.quantity = newQuantity;
+      const update = await ProductServices.updateSingleProduct(
+        productId,
+        OrderingProduct as TProducts
+      );
+      res.status(200).json({
+        success: true,
+        message: "Order created successfully!",
+        data: result,
+      });
+    }
   } catch (error: any) {
     res.status(500).json({
       success: false,
@@ -25,8 +51,10 @@ const getAllOrders = async (req: Request, res: Response) => {
   try {
     const { email } = req.query;
     let result;
+    // get products by search
     if (email) {
       result = await OrderServices.searchOrders(email);
+      // if no orders found for a specific email
       if (!result || result.length === 0) {
         return res.status(404).json({
           success: false,
@@ -34,6 +62,7 @@ const getAllOrders = async (req: Request, res: Response) => {
         });
       }
     } else {
+      // get all products
       result = await OrderServices.getAllOrders();
     }
     return res.status(200).json({
--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,3 +1,4 @@
+import { Products } from "../products/products.model";
 import { TOrders } from "./orders.interface";
 import { Orders } from "./orders.model";
 
