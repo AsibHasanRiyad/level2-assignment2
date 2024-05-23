ab696b232b2288f87c1ccfd61ff279d7873d8020
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-05-23 19:43:01 +0600

    Changes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use("/api/products", ProductRoutes);
 app.use("/api/orders", OrderRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Welcome to Order Management!");
 });
 
 app.use((req, res) => {
